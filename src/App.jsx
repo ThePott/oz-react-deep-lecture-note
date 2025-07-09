1f47fe68e1844d3ac4f62ea407d2046d1546cab5
Tynahan <nusilite@gmail.com>
2025-07-09 11:18:39 +0900

    01-1-SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="test">테스트용 텍스트
+
+
+
+        <div className="bgMuted">
+          <p className="fgMuted">Sample Text</p>
+          <p className="fg0">Sample Text</p>
+          <p className="fgAccent">Sample Text</p>
+        </div>
+
+        <div className="bg0">
+          <p className="fgMuted">Sample Text</p>
+          <p className="fg0">Sample Text</p>
+          <p className="fgAccent">Sample Text</p>
+        </div>
+
+        <div className="bgAccent">
+          <p className="fgMuted">Sample Text</p>
+          <p className="fg0">Sample Text</p>
+          <p className="fgAccent">Sample Text</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
