4396282adf6c32b8eb29a07b8b4d9f64de0eea82
Tynahan <nusilite@gmail.com>
2025-07-09 11:40:59 +0900

    01-2-SCSS-2

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,34 @@
 function App() {
   return (
     <>
-
       <div className="test">테스트용 텍스트
-
-
-
         <div className="bgMuted">
-          <p className="fgMuted">Sample Text</p>
-          <p className="fg0">Sample Text</p>
-          <p className="fgAccent">Sample Text</p>
+          <h1 className="fgMuted">Sample Text</h1>
+          <p>본문 예시</p>
+          <h1 className="fg0">Sample Text</h1>
+          <p>본문 예시</p>
+          <h1 className="fgAccent">Sample Text</h1>
+          <p>본문 예시</p>
         </div>
 
         <div className="bg0">
-          <p className="fgMuted">Sample Text</p>
-          <p className="fg0">Sample Text</p>
-          <p className="fgAccent">Sample Text</p>
+          <h1 className="fgMuted">Sample Text</h1>
+          <p>본문 예시</p>
+          <h1 className="fg0">Sample Text</h1>
+          <p>본문 예시</p>
+          <h1 className="fgAccent">Sample Text</h1>
+          <p>본문 예시</p>
         </div>
 
         <div className="bgAccent">
-          <p className="fgMuted">Sample Text</p>
-          <p className="fg0">Sample Text</p>
-          <p className="fgAccent">Sample Text</p>
+          <h1 className="fgMuted">Sample Text</h1>
+          <p>본문 예시</p>
+          <h1 className="fg0">Sample Text</h1>
+          <p>본문 예시</p>
+          <h1 className="fgAccent">Sample Text</h1>
+          <p>본문 예시</p>
         </div>
+
       </div>
 
     </>
