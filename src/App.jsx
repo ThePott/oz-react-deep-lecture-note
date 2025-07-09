7a4e7e4ea1132ca706d974364c7ec70a811095f4
Tynahan <nusilite@gmail.com>
2025-07-09 14:00:08 +0900

    01-5-Styled-Components-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import styled from "styled-components"
+import styled, { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+* {
+margin: 0;
+padding: 0;
+background-color: green;
+}
+`
 
 const BlueButton = styled.button`
-background-color: blue;
+background - color: blue;
 color: white;
 padding: 10px;
-border-radius: 4px;
+border - radius: 4px;
 width: 200px;
 `
 const BlueButtonBig = styled(BlueButton)`
-  font-size: 100px;
+font - size: 100px;
 `
-
+const PropsButton = styled.button`
+background - color: ${props => props.backgroundColor || "white"}
+`
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <div className="test">테스트용 텍스트
-        <BlueButton>yayaya</BlueButton>
-        <BlueButtonBig>yayaya</BlueButtonBig>
+        {/* <BlueButton>yayaya</BlueButton>
+        <BlueButtonBig>yayaya</BlueButtonBig> */}
+        <PropsButton backgroundColor="red">props button</PropsButton>
         <div className="bgMuted">
           <h1 className="fgMuted">Sample Text</h1>
           <p>본문 예시</p>
@@ -46,7 +58,6 @@ const App = () => {
         </div>
 
       </div>
-
     </>
   )
 }
